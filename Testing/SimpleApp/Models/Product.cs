ed397efce7eec656743d784fc33f90e632c79251
Nikita Luksha <nikita.luksha@softensity.com>
2021-10-28 19:26:01 +0300

    Set up MVC project

---
+++ Testing/SimpleApp/Models/Product.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimpleApp.Models
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public decimal? Price { get; set; }
+
+        public static Product[] GetProducts()
+        {
+            var kayak = new Product
+            {
+                Name = "Kayak",
+                Price = 275M
+            };
+
+            var lifejacket = new Product
+            {
+                Name = "Lifejacket",
+                Price = 48.95M
+            };
+
+            return new Product[] { kayak, lifejacket };
+        }
+    }
+}
