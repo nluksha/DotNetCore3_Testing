a9fb4e8657ed5f9cf67d788605ba17880ac48379
Nikita Luksha <nikita.luksha@softensity.com>
2021-10-28 19:57:28 +0300

    Init tests

---
+++ Testing/SimpleApp.Tests/ProductTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using SimpleApp.Models;
+
+namespace SimpleApp.Tests
+{
+    public class ProductTests
+    {
+        [Fact]
+        public void CanChangeProductName()
+        {
+            var p = new Product
+            {
+                Name = "Test",
+                Price = 100M
+            };
+
+            p.Name = "New Name";
+
+            Assert.Equal("New Name", p.Name);
+        }
+
+        [Fact]
+        public void CanChangeProductPrice()
+        {
+            var p = new Product
+            {
+                Name = "Test",
+                Price = 100M
+            };
+
+            p.Price = 200M;
+
+            Assert.Equal(100M, p.Price);
+        }
+    }
+}
